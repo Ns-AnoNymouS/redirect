e44cc9fc5c78b81d49cbe8073eef2fb9cefcb0ae
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2022-10-03 12:54:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,12 +6,16 @@ api_hash = os.environ.get("API_HASH")
 bot_tokens = [token for token in os.environ.get("BOT_TOKEN")]
 
 
+async def send_reply(c, m):
+    await m.reply_text("**Hey I am Back 😘\n\nPermanent shifted to 👉: @UploadXPro_Bot**", quote=True)
+
 async def main():
     apps = []
     i = 0
     for token in bot_tokens:
         i += 1
         x = Client(f"account{i}", bot_token=token, api_id=API_ID, api_hash=API_HASH)
+        x.add_handler(MessageHandler(send_reply))
         apps.append(x)
     
     await compose(apps)
