84c12f06657c222b031464aa219b8a7da3a1489c
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2022-10-03 13:02:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os, asyncio
 from pyrogram import Client, compose
+from pyrogram.handlers import MessageHandler
+
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
