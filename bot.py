9368646d4b384151a7d8048bbd1258e5241e7ae1
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2022-10-03 12:27:32 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_tokens = [token for token in os.environ.get("BOT_TOKEN")]
+
+
+if __name__ == '__main__':
+app = Client(
+    
+    api_id = int(os.environ.get("API_ID"))
+    api_hash = os.environ.get("API_HASH")
+
+)
